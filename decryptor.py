4f4ab2655117a0fa983e8d905ac4a6e1d330c01a
yawoffeh <82349700+yawoffeh@users.noreply.github.com>
2022-02-21 03:55:07 +0000

    Add files via upload

---
+++ decryptor.py
@@ -0,0 +1,17 @@
+import os
+import pyAesCrypt as pc
+
+files = [x for x in os.listdir() if x != "decryptor.py" and ".out" in x]
+def main():
+	passw = 'solstice'
+	filext = 'py'
+	bufferSize = 64 * 1024
+	for file in files:
+		try:
+			pc.decryptFile(file, file.replace("py.out", filext), passw, bufferSize)
+			print("decrypted")
+		except:
+			print("decryption unsucessful")
+
+if __name__ == '__main__':
+	main()
