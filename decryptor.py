32d2c63eaf5f6ef4641b47ffec4cc1547e001bb9
*Red <82349700+yawoffeh@users.noreply.github.com>
2022-05-16 07:22:46 +0000

    Update decryptor.py

--- decryptor.py
+++ decryptor.py
@@ -3,15 +3,15 @@ import pyAesCrypt as pc
 
 files = [x for x in os.listdir() if x != "decryptor.py" and ".out" in x]
 def main():
-	passw = 'solstice'
-	filext = 'py'
-	bufferSize = 64 * 1024
-	for file in files:
-		try:
-			pc.decryptFile(file, file.replace("py.out", filext), passw, bufferSize)
-			print("decrypted")
-		except:
-			print("decryption unsucessful")
+    passw = 'solstice'
+    filext = 'py'
+    bufferSize = 64 * 1024
+    for file in files:
+	try:
+	    pc.decryptFile(file, file.replace("py.out", filext), passw, bufferSize)
+	    print("decrypted")
+	except:
+	    print("decryption unsucessful")
 
 if __name__ == '__main__':
 	main()
